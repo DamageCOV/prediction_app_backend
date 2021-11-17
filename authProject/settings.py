2cbbc6709e87a50193b405c8dc42d5fab1bd2634
NataliaDurC <natu9715@gmail.com>
2021-11-17 07:49:12 -0500

    Autenticacion

--- authProject/settings.py
+++ authProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)jo6f8f+^okn+xk^915ow13lz2s=p&%c#ec5cxwka2%95&mazj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
